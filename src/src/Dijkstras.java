24d2b15d0fce16e09bb622c5b2267952fc674215
Kelvin Kinda <kchumbe17@alustudent.com>
2019-04-24 18:38:05 +0200

    runner's movement

--- src/src/Dijkstras.java
+++ src/src/Dijkstras.java
@@ -7,14 +7,17 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Dijkstras {
+    // function to search the runners path using dijkstra's algorithm to the goal node considering the weight of the nodes
     @SuppressWarnings("Duplicates")
     public static void Dijkstras_Search_8D(Node[][] grid_nodes, Node start, Node goal, HashMap<Integer,Image>image_dict) throws IOException {
+        // set to store all unvisited nodes
         Set<Node> unvisited_nodes = new HashSet<>();
-        Set<Node> traced_path = new HashSet<>();
-        traced_path.add(start);
 
+        // update all nodes in the grid node. Set their gscore to negative infinity and update their neighbours if they are valid i.e are not blocked and their coordinates are valid
         for(int i=0; i < grid_nodes.length; i++){
             for(int j=0; j < grid_nodes[i].length; j++){
+                grid_nodes[i][j].setgScore((int) Double.NEGATIVE_INFINITY);
+
                 if(A_Star.cell_isValid(grid_nodes,i-1, j-1) && !A_Star.cell_isBlocked(grid_nodes, i-1, j-1, image_dict)){
                     grid_nodes[i][j].addNeighbours(grid_nodes[i-1][j-1]);
                 }
@@ -40,26 +43,27 @@ public class Dijkstras {
                     grid_nodes[i][j].addNeighbours(grid_nodes[i+1][j+1]);
                 }
 
+                // update the grid nodes energy cost
                 for(Integer key: image_dict.keySet()){
                     if(image_dict.get(key).equals(grid_nodes[i][j].getImageView().getImage())){
                         switch (key){
                             case 0:
-                                grid_nodes[i][j].energy_cost = 8;
+                                grid_nodes[i][j].energy_cost = -1;
                                 break;
                             case 2:
-                                grid_nodes[i][j].energy_cost = 7;
+                                grid_nodes[i][j].energy_cost = -2;
                                 break;
                             case 3:
-                                grid_nodes[i][j].energy_cost = 5;
+                                grid_nodes[i][j].energy_cost = -4;
                                 break;
                             case 4:
-                                grid_nodes[i][j].energy_cost = 1;
+                                grid_nodes[i][j].energy_cost = -8;
                                 break;
                             case 5:
-                                grid_nodes[i][j].energy_cost = 10;
+                                grid_nodes[i][j].energy_cost = 1;
                                 break;
                             case 6:
-                                grid_nodes[i][j].energy_cost = 14;
+                                grid_nodes[i][j].energy_cost = 5;
                                 break;
                         }
                     }
@@ -67,14 +71,20 @@ public class Dijkstras {
             }
         }
 
+        // add all nodes to the unvisited set
         for(Node[] node_arr: grid_nodes){
             unvisited_nodes.addAll(Arrays.asList(node_arr));
         }
 
+        // update the start's gscore to 0
         start.setgScore(0);
+
         boolean foundDestination = false;
 
+        // there's still nodes in the unvisited set and we havent found the destination
         while(!unvisited_nodes.isEmpty() && !foundDestination){
+
+            // find the node with the largest gscore and set it to our current
             Node current = null;
             int max = (int) Double.NEGATIVE_INFINITY;
             for(Node node: unvisited_nodes){
@@ -84,48 +94,52 @@ public class Dijkstras {
                 }
             }
 
+            // remove the current node from the unvisited set
             unvisited_nodes.remove(current);
-//            traced_path.add(current);
 
-
+            // get the current's nodes position on the grid
             int[] current_pos = A_Star.identify_CellPos(current, grid_nodes);
             current.posOnGrid = current_pos;
 
-
+            // check if our current node is our destination
             if(A_Star.cell_isDestination(grid_nodes, current_pos[0], current_pos[1], image_dict)){
                 A_Star.write_constructedPath(current, start, goal);
                 foundDestination = true;
                 break;
             }
 
-
+            // for each of the current's neighbours
             for(Node neighbour: current.getNeighbours()){
-                int temp_Score = current.getgScore() + (neighbour.energy_cost - current.energy_cost);
 
-                if(temp_Score > neighbour.getgScore()){
-                    neighbour.setgScore(temp_Score);
-                    neighbour.parent = current;
+                // check if the neighbour has not been explored yet
+                if(unvisited_nodes.contains(neighbour)){
+
+                    // if so, calculate the temp score by adding the current's gscore and the neighbour's energy cost
+                    int temp_Score = current.getgScore() + neighbour.energy_cost;
+
+                    // if the temp score is greater than the neighbour's score, update the neighbour's gscore to the temp score and update its parent to be the current node
+                    if(temp_Score > neighbour.getgScore()){
+                        neighbour.setgScore(temp_Score);
+                        neighbour.parent = current;
+                    }
                 }
             }
-
-
-
         }
+
+        // if the unvisited set is empty and we havent found our destination, alert the user
         if(unvisited_nodes.isEmpty() && !foundDestination){
             System.out.println("Could Not Find the Destination");
         }
-
     }
 
-
+    // function to search the runners path using dijkstra's algorithm to the goal node considering the weight of the nodes
     @SuppressWarnings("Duplicates")
     public static void Dijkstras_Search_4D(Node[][] grid_nodes, Node start, Node goal, HashMap<Integer,Image>image_dict) throws IOException {
         Set<Node> unvisited_nodes = new HashSet<>();
-//        Set<Node> traced_path = new HashSet<>();
-//        traced_path.add(start);
 
         for(int i=0; i < grid_nodes.length; i++){
             for(int j=0; j < grid_nodes[i].length; j++){
+                grid_nodes[i][j].setgScore((int) Double.NEGATIVE_INFINITY);
 
                 if(A_Star.cell_isValid(grid_nodes, i-1, j) && !A_Star.cell_isBlocked(grid_nodes, i-1, j, image_dict)){
                     grid_nodes[i][j].addNeighbours(grid_nodes[i-1][j]);
@@ -200,20 +214,19 @@ public class Dijkstras {
 
 
             for(Node neighbour: current.getNeighbours()){
-                int temp_Score = current.getgScore() + (current.getgScore() + neighbour.energy_cost);
+                if(unvisited_nodes.contains(neighbour)){
+                    int temp_Score = current.getgScore() +  neighbour.energy_cost;
 
-                if(temp_Score > neighbour.getgScore()){
-                    neighbour.setgScore(temp_Score);
-                    neighbour.parent = current;
+                    if(temp_Score > neighbour.getgScore()){
+                        neighbour.setgScore(temp_Score);
+                        neighbour.parent = current;
+                    }
                 }
+
             }
-
-
-
         }
         if(unvisited_nodes.isEmpty() && !foundDestination){
             System.out.println("Could Not Find the Destination");
         }
-
     }
 }
