50ae882b025b786814b1bfeac3f286155bad612a
Kelvin Kinda <kchumbe17@alustudent.com>
2019-04-25 02:50:26 +0200

    updated complexities on Dijkstras methods

--- src/src/Dijkstras.java
+++ src/src/Dijkstras.java
@@ -7,7 +7,13 @@ import java.util.HashSet;
 import java.util.Set;
 
 // dijkstras
+// Methods used to determine validity of some aspects have been borrowed from static methods in the A_Star class
 public class Dijkstras {
+    /**
+     Time Complexity: O(N^2)
+     Space Complexity: O(N^2)
+     Auxiliary Space: O(N)
+     */
     // function to search the runners path using dijkstra's algorithm to the goal node considering the weight of the nodes
     @SuppressWarnings("Duplicates")
     public static void Dijkstras_Search_8D(Cell_Node[][] grid_nodes, Cell_Node start, Cell_Node goal, HashMap<Integer,Image>image_dict) throws IOException {
@@ -133,11 +139,17 @@ public class Dijkstras {
         }
     }
 
+    /**
+     Time Complexity: O(N^2)
+     Space Complexity: O(N^2)
+     Auxiliary Space: O(N)
+     */
     // function to search the runners path using dijkstra's algorithm to the goal node considering the weight of the nodes
     @SuppressWarnings("Duplicates")
     public static void Dijkstras_Search_4D(Cell_Node[][] grid_nodes, Cell_Node start, Cell_Node goal, HashMap<Integer,Image>image_dict) throws IOException {
         Set<Cell_Node> unvisited_nodes = new HashSet<>();
 
+        // update the grid nodes's gscore, their neighbours and their energy costs
         for(int i=0; i < grid_nodes.length; i++){
             for(int j=0; j < grid_nodes[i].length; j++){
                 grid_nodes[i][j].setgScore((int) Double.NEGATIVE_INFINITY);
@@ -182,14 +194,19 @@ public class Dijkstras {
             }
         }
 
+        // add all nodes in the grid to the unvisited set
         for(Cell_Node[] node_arr: grid_nodes){
             unvisited_nodes.addAll(Arrays.asList(node_arr));
         }
 
+        // set the start nodes gscore to 0
         start.setgScore(0);
         boolean foundDestination = false;
 
+        // while there's still nodes that are univisited..
         while(!unvisited_nodes.isEmpty() && !foundDestination){
+
+            // set the current node to be the node with the highest gscore
             Cell_Node current = null;
             int max = (int) Double.NEGATIVE_INFINITY;
             for(Cell_Node node: unvisited_nodes){
@@ -199,21 +216,22 @@ public class Dijkstras {
                 }
             }
 
+            // remove the current node from the unvisited set
             unvisited_nodes.remove(current);
-//            traced_path.add(current);
 
-
+            // get currents position on the grid
             int[] current_pos = A_Star.identify_CellPos(current, grid_nodes);
             current.posOnGrid = current_pos;
 
 
+            // check if current is our destination
             if(A_Star.cell_isDestination(grid_nodes, current_pos[0], current_pos[1], image_dict)){
                 A_Star.write_constructedPath(current, start, goal);
                 foundDestination = true;
                 break;
             }
 
-
+            // for each of current's neighbour, update their gscore and parents
             for(Cell_Node neighbour: current.getNeighbours()){
                 if(unvisited_nodes.contains(neighbour)){
                     int temp_Score = current.getgScore() +  neighbour.energy_cost;
